2d8643420fbc80d9c367729b16501a15372aeb4d
Onesco <Okonkwoonesmus5@gmail.com>
2022-07-14 22:23:53 +0100

    up date the setting file to include include get routes method

--- package/config/setting.js
+++ package/config/setting.js
@@ -4,6 +4,7 @@ const {dirname, join } = require("path");
 
 const getRootPath = require("../util/getRootPath")
 const getMetadata = require("./getMetadata")
+const getRegisteredRoutesTemplete = require("./getRegisteredRoutes");    
 
 
 const config = (...options)=>{
@@ -34,7 +35,6 @@ const config = (...options)=>{
     })
  
    
-
    if(!fs.existsSync(autoGenPath)){
        fs.mkdirSync(autoGenPath, {recursive:true})
        
@@ -62,25 +62,43 @@ const config = (...options)=>{
         fs.readFile(join(autoGenPath, "swaggerDocument.json"), "utf8", (err, file)=>{
             if(err)console.log(err)
             app.use(`/${docs}`, swaggerUi.serve, swaggerUi.setup(JSON.parse(file)))
-        }) 
-
-       
+        })    
    }
-    else{
-        if (environment=== "production"){
-            let file = require(join(autoGenPath, "swaggerDocument.json"))
-            app.use(`/${docs}`, swaggerUi.serve, swaggerUi.setup(
-                file
-            ))
-        }
-        else{
-            fs.readFile(join(autoGenPath, "swaggerDocument.json"), "utf8", (err, file)=>{
-                if(err)console.log(err)
-                app.use(`/${docs}`, swaggerUi.serve, swaggerUi.setup(JSON.parse(file)))
-            }) 
-        }
+
+    let jsonSchema =  fs.readFileSync(join(autoGenPath, "jsonSchema.json"))
+    jsonSchema = JSON.parse(jsonSchema)
+    const [routes, tem,  routesObjs] = getRegisteredRoutesTemplete(app)
+    
+    
+    if(tem && Object.keys(jsonSchema).length < 1){
+          
+        let optem = JSON.parse(tem)
+        
+        app.use(`/${docs}`, function(req, res, next){
+            let host = req.get('host');
+            if(optem.servers.length === 0){
+                optem.servers = [{url:'http://'+ host}];
+            }
+            req.swaggerDoc = optem;
+            next();
+        }, swaggerUi.serveFiles(optem), swaggerUi.setup());
     }
 
+    let currentTemplate = JSON.parse(fs.readFileSync(join(autoGenPath, "swaggerDocument.json")))
+    app.use(`/${docs}`,swaggerUi.serve, swaggerUi.setup(currentTemplate))
+    
+    if(Object.keys(jsonSchema).length > 0) {
+        
+        // const jsonschemaKeyList =  Object.keys(jsonSchema)
+        // const jsonschemaKeyListSet = new Set(jsonschemaKeyList)
+
+        // const routeListSet = new Set(routesList)
+        // let routeToRemove = new Set([...jsonschemaKeyListSet].filter(x => !routeListSet.has(x)));
+        // console.log(routesObjs)
+        console.log(currentTemplate.paths)
+        // console.log(routes)
+    }
+    
 }
 
 module.exports ={
